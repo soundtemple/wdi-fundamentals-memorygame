4e590597c974b532550004c376d9182dbb152856
soundtemple <twwalter@gmail.com>
2016-09-16 06:57:24 +1000

    update game logic unit9 assign

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,20 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+// Assignment instructions to test against two kings, two queens, one of each
+if (cardTwo === "king" && cardFour === "king") {
+  alert('You found a match. Two kings!');
+} else if (cardTwo === "queen" && cardFour === "queen") {
+  alert('You found a match. Two queens!');
+} else {
+	alert('Sorry. Try again.')
+}
+
+
+// Simpler code just testing strict equality
+// if (cardThree === cardFour) {
+// 	alert('You found a match!')
+// } else {
+// 	alert('Sorry. Try again.')
+// }
