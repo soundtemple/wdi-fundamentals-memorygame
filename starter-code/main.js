cf085d1d203a146d038d3f976621af5f9973df54
soundtemple <twwalter@gmail.com>
2016-09-20 16:25:37 +1000

    update javascript unit10 assign

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,16 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
+
+
 
 // Assignment instructions to test against two kings, two queens, one of each
-if (cardTwo === "king" && cardFour === "king") {
-  alert('You found a match. Two kings!');
-} else if (cardTwo === "queen" && cardFour === "queen") {
-  alert('You found a match. Two queens!');
-} else {
-	alert('Sorry. Try again.')
-}
+// if (cardTwo === "king" && cardFour === "king") {
+//   alert('You found a match. Two kings!');
+// } else if (cardTwo === "queen" && cardFour === "queen") {
+//   alert('You found a match. Two queens!');
+// } else {
+// 	alert('Sorry. Try again.')
+// }
 
 
 // Simpler code just testing strict equality
@@ -19,3 +21,17 @@ if (cardTwo === "king" && cardFour === "king") {
 // } else {
 // 	alert('Sorry. Try again.')
 // }
+
+// Get an element with the id of game-board and set it to a variable.
+var createBoard = document.getElementById('game-board');
+
+function createBoard() { //function to create board
+	for (var i=0; i<cards.length; i++) { //loop to create card elements
+		var cardElement = document.createElement('div'); //create the card
+	cardElement.className = 'card'; //add the class 'card' to the card element
+	board.appendChild(cardElement); //append the card to the board
+
+	}
+}
+
+
