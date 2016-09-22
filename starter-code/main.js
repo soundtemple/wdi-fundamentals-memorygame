a2a3234c9f2511a909a15e8aacb54f86c609e1c2
soundtemple <twwalter@gmail.com>
2016-09-22 12:18:58 +1000

    update unit11 assign

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,3 +1,4 @@
+// CODE FROM EARLIER ASSIGNMENTS
 // var cardOne = "queen";
 // var cardTwo = "queen";
 // var cardThree = "king";
@@ -21,16 +22,79 @@
 // } else {
 // 	alert('Sorry. Try again.')
 // }
+//  **************************************************
+
+
+var cards = ['queen', 'queen', 'king', 'king' ]; // set cards of the game
+var cardsInPlay = []; // cards in play
+var scoreCount = 0;
+
 
 // Get an element with the id of game-board and set it to a variable.
 var board = document.getElementById('game-board');
+var score = document.getElementById('score');
+var message = document.getElementById('message');
 
 function createBoard() { //function to create board
 	for (var i=0; i<cards.length; i++) { //loop to create card elements
 		var cardElement = document.createElement('div'); //create the card
-	cardElement.className = 'card'; //add the class 'card' to the card element
-	board.appendChild(cardElement); //append the card to the board
+		cardElement.setAttribute('data-card', cards[i]);  // this will set the card's 'data-card' to be the element of the array
+		cardElement.className = 'card'; //add the class 'card' to the card element
+		cardElement.addEventListener('click', isTwoCards);
+		board.appendChild(cardElement); //append the card to the board
 
 	}
 }
+
+
+function isTwoCards() {
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this prework, but
+  // for now, just know it gives you access to the card the user clicked on
+  cardsInPlay.push(this.getAttribute('data-card'));
+  	console.log(this.getAttribute('data-card'));
+  	if (this.getAttribute('data-card') === 'king') {
+  		this.innerHTML = "<img src='images/king.jpg'>"; 
+  	} else {
+  		this.innerHTML = "<img src='images/queen.jpg'>";
+  	}
+  	 // if you have two cards in play check for a match
+	if (cardsInPlay.length === 2) {
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+    // clear cards in play array for next try
+    cardsInPlay = [];
+	}
+  }
+  
+ 
+
+function isMatch(cards) {
+  // alert winning message
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    scoreCount +=1;
+    score.innerHTML = ("<h4>Score: " + scoreCount + "</h4>");
+    message.innerHTML = ("<h4>You found a match!</h4>");
+      } else {
+    message.innerHTML = ("<h4>Sorry. Try again.</h4>");
+
+  }
+  if (scoreCount === 5) {
+  	scoreCount = 0;
+  	score.innerHTML = ("<h4>Score: " + scoreCount + "</h4>");
+  	message.innerHTML = ("<h4>GAME OVER!!! Play again?</h4>");
+  	resetGame();
+  }
+}
+
+function resetGame() {
+	for (var i = 0; i < cards.length; i++) {
+		document.getElementsByClassName('card')[i].innerHTML = " ";
+	}
+}
+
+createBoard();
+
+
+
 
